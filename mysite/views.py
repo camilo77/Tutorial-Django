b196585cd9e4053ef4020f013fc34fa89d2367c8
Camilo77 <camilopineda77@hotmail.com>
2014-06-20 12:29:17 -0500

    agregando archivos faltantes para el commit anterior

--- mysite/views.py
+++ mysite/views.py
@@ -14,14 +14,14 @@ def hello(request):
 #	return HttpResponse(html)
 
 # APLICANDO METODO RENDER TO RESPONSE para la vista anterior
-#def current_datetime(request):
-#	now = datetime.datetime.now()
-#	return render_to_response('current_datetime.html', {'current_date':now})
+def current_datetime(request):
+	now = datetime.datetime.now()
+	return render_to_response('current_datetime.html', {'current_date':now})
 
 # APLICANDO EL METODO DE LOCALS PARA MAPEAR TODAS LAS VARIABLES LOCALES DE LA VISTA Y AHORRAR CODIGO
-def current_datetime(request):
-	current_date = datetime.datetime.now()
-	return render_to_response('current_datetime.html',locals()) 
+#def current_datetime(request):
+#	current_date = datetime.datetime.now()
+#	return render_to_response('current_datetime.html',locals()) 
 
 def hours_ahead(request, offset):
 	try:
@@ -29,5 +29,4 @@ def hours_ahead(request, offset):
 	except ValueError:
 		raise Http404()
 	dt = datetime.datetime.now() + datetime.timedelta(hours = offset)
-	html = "<html><body>in %s hours, it will be %s.</body></html>" %(offset, dt)
-	return HttpResponse(html)
+	return render_to_response('hours_ahead.html', {'hours_offset':offset,'next_time':dt})
