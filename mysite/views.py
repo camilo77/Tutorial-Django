edfedbdc24679c0cdd5adff977e970cda290af44
Camilo77 <camilopineda77@hotmail.com>
2014-06-20 11:48:51 -0500

    modificando vista con el metodo render_to_response

--- mysite/views.py
+++ mysite/views.py
@@ -1,16 +1,22 @@
 from django.http import HttpResponse
 from django.template.loader import get_template
 from django.template import Context
+from django.shortcuts import render_to_response
 import datetime
 
 def hello(request):
 	return HttpResponse("hello word")
 
+#def current_datetime(request):
+#	now = datetime.datetime.now()
+#	t = get_template('current_datetime.html')
+#	html = t.render(Context({'current_date':now}))
+#	return HttpResponse(html)
+
+# APLICANDO METODO RENDER TO RESPONSE para la vista anterior
 def current_datetime(request):
 	now = datetime.datetime.now()
-	t = get_template('current_datetime.html')
-	html = t.render(Context({'current_date':now}))
-	return HttpResponse(html)
+	return render_to_response('current_datetime.html', {'current_date':now})
 
 def hours_ahead(request, offset):
 	try:
