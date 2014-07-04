6462035b3ec1f607210274cf6411dc977339247a
Camilo77 <camilopineda77@hotmail.com>
2014-07-04 11:27:21 -0500

    tutoriales terminados

--- mysite2/polls/admin.py
+++ mysite2/polls/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from models import *
+
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3
+
+#midificando apariencia del admin para las preguntas
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None, 					{'fields': ['question']}),
+		('date information', 	{'fields': ['pub_date'], 'classes': ['collapse']})
+	]
+	# agregando respuestas a preguntas
+	inlines = [ChoiceInline]
+	# modificando que campos mostrar en las tablas
+	list_display = ('id','question', 'pub_date', 'was_published_recently')
+	# par filtrar y orden segun:
+	list_filter = ['pub_date']
+	search_field = ['question']
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
 
 # Register your models here.
--- mysite2/polls/models.py
+++ mysite2/polls/models.py
@@ -1,11 +1,27 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
-	question = models.CharField(max_length = 200)
+	question = models.CharField('pregunta', max_length = 200)
 	pub_date = models.DateTimeField('date published')
+
+	def __str__(self):
+		return self.question
+
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
+	was_published_recently.admin_order_field = 'pub_date'
+	was_published_recently.boolean = 'true'
+	was_published_recently.short_description = 'published rencently?'
+	
+
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question)
 	choice_text = models.CharField(max_length = 200)
 	votes = models.IntegerField(default = 0)
+
+	def __str__(self):
+		return self.choice_text
