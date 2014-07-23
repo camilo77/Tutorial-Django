bae9967565b020d67df2eaf226add092e16ab877
Camilo77 <camilopineda77@hotmail.com>
2014-07-22 22:21:09 -0500

    fin tutorial 2

--- mysite2/polls/models.py
+++ mysite2/polls/models.py
@@ -4,8 +4,8 @@ from django.utils import timezone
 
 # Create your models here.
 class Poll(models.Model):
-	question = models.CharField('pregunta', max_length = 200)
-	pub_date = models.DateTimeField('date published')
+	question = models.CharField('Pregunta', max_length = 200)
+	pub_date = models.DateTimeField('Fecha de publicacion')
 
 	def __unicode__(self):
 		return self.question
@@ -15,6 +15,7 @@ class Poll(models.Model):
 
 	def was_published_recently(self):
 		return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
+
 	was_published_recently.admin_order_field = 'pub_date'
 	was_published_recently.boolean = 'true'
 	was_published_recently.short_description = 'published rencently?'
@@ -23,11 +24,11 @@ class Poll(models.Model):
 
 class Choice(models.Model):
 	question = models.ForeignKey(Poll)
-	choice_text = models.CharField(max_length = 200)
-	votes = models.IntegerField(default = 0)
+	choice_text = models.CharField('Opcion',max_length = 200)
+	votes = models.IntegerField('No. votos',default = 0)
 
 	def __unicode__(self):
 		return self.choice_text
-		
+
 	def __str__(self):
 		return self.choice_text
