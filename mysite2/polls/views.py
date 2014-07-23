c2cb94d6c1ad1524a723e420ca637d3540db7269
Camilo77 <camilopineda77@hotmail.com>
2014-07-22 23:51:43 -0500

    A shorcut: render()

--- mysite2/polls/views.py
+++ mysite2/polls/views.py
@@ -5,13 +5,19 @@ from django.template import RequestContext, loader
 
 # Create your views here.
 
+#def index(request):
+#	latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+#	template = loader.get_template('polls/index.html')
+#	context = RequestContext(request, {
+#		'latest_poll_list': latest_poll_list,
+#	})
+#	return HttpResponse(template.render(context))
+
+# version mejorada
 def index(request):
-	latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-	template = loader.get_template('polls/index.html')
-	context = RequestContext(request, {
-		'latest_poll_list': latest_poll_list,
-	})
-	return HttpResponse(template.render(context))
+	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+	context = {'latest_poll_list':latest_poll_list}
+	return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
 	return HttpResponse("you are looking at poll %s" %poll_id)
